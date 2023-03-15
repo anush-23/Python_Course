3237454fe20d80ba1583c23cfb5674b6c22e341f
anush.23 <anushitik@gmail.com>
2023-03-16 00:18:38 +0400

    Final homework 14.03

--- Homework/homework_14.03.py
+++ Homework/homework_14.03.py
@@ -1,47 +1,59 @@
 """For this challenge, you are supposed to find the sum of the digits of a two-digit number."""
 
-x = int(input("write a number to get the sum of its digits:  "))
-print(str(x) + ' -> ' + str(x % 10 + x // 10))
-
+x = int(input("write a two digit number to get the sum of its digits:  "))
+if (10<x<99):
+    print(str(x) + ' -> ' + str(x % 10 + x // 10))
+else:
+    print("Please enter a two digit number")
 
 
 
 """A repdigit is a positive number composed out of the same digit. Create a function that takes an two-digit integer and returns whether it's a repdigit or not."""
 
-x = int(input("write a number to check if it is repdigit:  "))
-print(str(x) + ' -> ' + str(x % 10 == x // 10))
+x = int(input("write a two digit number to check if it is repdigit:  "))
+if (10<x<99):
+    print(str(x) + ' -> ' + str(x % 10 == x // 10))
+else:
+    print("Please enter a two digit number")
 
 
 """ Write a function that takes an integer minutes and converts it to seconds."""
 
-x = int(input("write minutes to convert it into seconds:  "))
-print(str(x) + ' -> ' + str(x*60))
+def minutesToSecondsConverter(x):
+    print(str(x) + ' -> ' + str(x*60))
 
+minutesToSecondsConverter(5)
+minutesToSecondsConverter(21)
 
 
 """Create a function that takes the age in years and returns the age in days."""
 
-x = int(input("write the age in years to get it in days:  "))
-print(str(x) + ' -> ' + str(x*365))
+def yearsToDays(x):
+    print(str(x) + ' -> ' + str(x*365))
+
+yearsToDays(15)
 
 
 """Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet"""
+def inchToFeetConverter(x):
+    print(str(x) + ' -> ' + str(x*(1/12)))
 
-x = float(input("write inches to convert into feet:  "))
-print(str(x) + ' -> ' + str(x*(1/12)))
+inchToFeetConverter(1.5)
+inchToFeetConverter(5)
 
 """Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers."""	
-x = float(input("write a number to check if it is odd or even:  "))
+def oddVSeven(number):
+    if (number % 2 == 0):
+        print("even")
+    else:
+        print("odd")
 
-if (x % 2 == 0):
-    print("even")
-else:
-    print("odd")
+oddVSeven(8)
+oddVSeven(7)
 
 
 """Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them."""
+def timeToSecondsConverter(hours, minutes):
+    print(str(hours) + "," + str(minutes) + ' -> ' + str(hours*3600+minutes*60))
 
-x = int(input("write the number for hours:  "))
-y = int(input("write the number for minutes:  "))
-
-print(str(x) + "," + str(y) + ' -> ' + str(x*3600+y*60))
+timeToSecondsConverter(3,5)
